929afb64badb23e7162037807cad86d3b9474f6c
mlemon <meng004@gmail.com>
2013-09-30 15:41:28 +0800

    增加EF与Mongo的仓储实现

---
+++ TextbookManage.Repository/EntityFramework/CategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using ByteartRetail.Domain.Model;
+
+namespace ByteartRetail.Domain.Repositories.EntityFramework
+{
+    public class CategoryRepository : EntityFrameworkRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(IRepositoryContext context) : base(context) { }
+    }
+}
---
+++ TextbookManage.Repository/EntityFramework/RoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using ByteartRetail.Domain.Model;
+
+namespace ByteartRetail.Domain.Repositories.EntityFramework
+{
+    public class RoleRepository : EntityFrameworkRepository<Role>, IRoleRepository
+    {
+        public RoleRepository(IRepositoryContext context)
+            : base(context)
+        { }
+
+    }
+}
