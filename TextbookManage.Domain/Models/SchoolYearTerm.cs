ad28654fea3e2792582a6fa29618898d14bf67ae
mlemon <meng004@gmail.com>
2014-06-03 12:13:24 +0800

    修改学年学期

---
+++ TextbookManage.Domain/Models/SchoolYearTerm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TextbookManage.Domain.Models
+{
+    /// <summary>
+    /// 学年学期
+    /// 结构体，当作值对象
+    /// </summary>
+    public class SchoolYearTerm
+    {
+        /// <summary>
+        /// 学年
+        /// </summary>
+        public string Year { get; set; }
+        /// <summary>
+        /// 学期
+        /// </summary>
+        public string Term { get; set; }
+    }
+}
