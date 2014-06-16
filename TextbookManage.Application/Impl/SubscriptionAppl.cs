d9c5d9264f11cd29dcc3882a6e5cff8e7c3d34ce
lemon <meng004@gmail.com>
2014-06-16 11:41:18 +0800

    增加取未征订用书申报

--- TextbookManage.Application/Impl/SubscriptionAppl.cs
+++ TextbookManage.Application/Impl/SubscriptionAppl.cs
@@ -50,172 +50,230 @@ namespace TextbookManage.Applications.Impl
 
         public IEnumerable<SubscriptionForSubmitView> CreateSubscriptionByTextbook(string textbookName, string isbn)
         {
-            var term = new TermAppl().GetMaxTerm();
+            return new List<SubscriptionForSubmitView>();
+            //var term = new TermAppl().GetMaxTerm();
 
-            var yearTerm = new SchoolYearTerm(term.YearTerm);
+            //var yearTerm = new SchoolYearTerm(term.YearTerm);
 
-            //取全部未下订单的申报
-            var query = _stuDeclJiaoWuRepo.Find(t =>
-                t.SchoolYearTerm.Year == yearTerm.Year &&
-                t.SchoolYearTerm.Term == yearTerm.Term
-                ).OfType<StudentDeclaration>()
+            ////取全部未下订单的申报
+            //var query = _stuDeclJiaoWuRepo.Find(t =>
+            //    t.SchoolYearTerm.Year == yearTerm.Year &&
+            //    t.SchoolYearTerm.Term == yearTerm.Term
+            //    ).OfType<StudentDeclaration>()
 
-                .Where(t =>
-                    !t.Subscription_Id.HasValue
-                    ).Where(t =>
-                        t.Textbook_Id.HasValue
-                        );
-            IEnumerable<Declaration> declarations = new List<Declaration>();
-            //按教材名称或ISBN模糊查询
-            if (string.IsNullOrWhiteSpace(textbookName))
-            {
-                if (string.IsNullOrWhiteSpace(isbn))
-                {
+            //    .Where(t =>
+            //        !t.Subscription_Id.HasValue
+            //        ).Where(t =>
+            //            t.Textbook_Id.HasValue
+            //            );
+            //IEnumerable<Declaration> declarations = new List<Declaration>();
+            ////按教材名称或ISBN模糊查询
+            //if (string.IsNullOrWhiteSpace(textbookName))
+            //{
+            //    if (string.IsNullOrWhiteSpace(isbn))
+            //    {
 
-                }
-                else
-                {
-                    declarations = query.Where(t => t.Textbook.Isbn.Contains(isbn));
-                }
-            }
-            else
-            {
-                declarations = query.Where(t => t.Textbook.Name.Contains(textbookName));
-            }
-            //由申报创建订单
-            var subs = Domain.SubscriptionService.CreateSubscriptions(declarations);
-            subs = subs.OrderBy(t => t.Textbook.Name);
-            var result = _adapter.Adapt<SubscriptionForSubmitView>(subs);
-            return result;
+            //    }
+            //    else
+            //    {
+            //        declarations = query.Where(t => t.Textbook.Isbn.Contains(isbn));
+            //    }
+            //}
+            //else
+            //{
+            //    declarations = query.Where(t => t.Textbook.Name.Contains(textbookName));
+            //}
+            ////由申报创建订单
+            //var subs = Domain.SubscriptionService.CreateSubscriptions(declarations);
+            //subs = subs.OrderBy(t => t.Textbook.Name);
+            //var result = _adapter.Adapt<SubscriptionForSubmitView>(subs);
+            //return result;
         }
 
         public IEnumerable<SchoolView> GetSchoolWithNotSub()
         {
-            var term = new TermAppl().GetMaxTerm().YearTerm;
-            //取学生申报，取学生学院
-            var stuDecl = _stuDeclRepo.Find(t =>
-                t.Term == term &&
-                t.ApprovalState == ApprovalState.终审通过
-                ).Where(t =>
-                    !t.Subscription_Id.HasValue
-                    );
+            return new List<SchoolView>();
+            //var term = new TermAppl().GetMaxTerm().YearTerm;
+            ////取学生申报，取学生学院
+            //var stuDecl = _stuDeclRepo.Find(t =>
+            //    t.Term == term &&
+            //    t.ApprovalState == ApprovalState.终审通过
+            //    ).Where(t =>
+            //        !t.Subscription_Id.HasValue
+            //        );
 
-            var stuSchool = stuDecl.SelectMany(t =>
-                t.DeclarationClasses
-                ).Select(t =>
-                    t.ProfessionalClass.School
-                    ).Distinct();
+            //var stuSchool = stuDecl.SelectMany(t =>
+            //    t.DeclarationClasses
+            //    ).Select(t =>
+            //        t.ProfessionalClass.School
+            //        ).Distinct();
 
-            //取教师申报，取教师学院
-            var teaDecl = _teaDeclRepo.Find(t =>
-                t.Term == term &&
-                t.ApprovalState == ApprovalState.终审通过
-                ).Where(t =>
-                    !t.Subscription_Id.HasValue
-                    );
+            ////取教师申报，取教师学院
+            //var teaDecl = _teaDeclRepo.Find(t =>
+            //    t.Term == term &&
+            //    t.ApprovalState == ApprovalState.终审通过
+            //    ).Where(t =>
+            //        !t.Subscription_Id.HasValue
+            //        );
 
-            var teaSchool = teaDecl.Select(t =>
-                t.TeachingTask.Department.School
-                ).Distinct();
-            //合并
-            var concatSchools = stuSchool.Union(teaSchool).Distinct(new TextbookManage.Domain.Comparer.SchoolComparer());
-            IList<School> schools = concatSchools.Distinct().OrderBy(t => t.Name).ToList();
+            //var teaSchool = teaDecl.Select(t =>
+            //    t.TeachingTask.Department.School
+            //    ).Distinct();
+            ////合并
+            //var concatSchools = stuSchool.Union(teaSchool).Distinct(new TextbookManage.Domain.Comparer.SchoolComparer());
+            //IList<School> schools = concatSchools.Distinct().OrderBy(t => t.Name).ToList();
 
-            //检查
-            if (schools.Count() == 0)
-            {
-                schools = new List<School>()
-                {
-                    new School()
-                    {
-                        SchoolId=default(Guid),
-                        Name="没有未下订单的学院"
-                    }
-                };
-            }
-            return _adapter.Adapt<SchoolView>(schools);
+            ////检查
+            //if (schools.Count() == 0)
+            //{
+            //    schools = new List<School>()
+            //    {
+            //        new School()
+            //        {
+            //            SchoolId=default(Guid),
+            //            Name="没有未下订单的学院"
+            //        }
+            //    };
+            //}
+            //return _adapter.Adapt<SchoolView>(schools);
 
         }
 
         public IEnumerable<SubscriptionForSubmitView> CreateSubscriptionBySchoolId(string schoolId)
         {
-            var id = schoolId.ConvertToGuid();
-            var term = new TermAppl().GetMaxTerm().YearTerm;
-            //取未下订单的学生申报
-            IEnumerable<Declaration> stuDecl = _stuDeclRepo.Find(t =>
-                t.Term == term &&
-                t.ApprovalState == ApprovalState.终审通过
-                ).Where(t =>
-                    !t.Subscription_Id.HasValue
-                    ).SelectMany(t =>
-                        t.DeclarationClasses
-                        ).Where(t =>
-                            t.ProfessionalClass.School_Id == id
-                            ).Select(t =>
-                                t.Declaration
-                                );
+            return new List<SubscriptionForSubmitView>();
+            //var id = schoolId.ConvertToGuid();
+            //var term = new TermAppl().GetMaxTerm().YearTerm;
+            ////取未下订单的学生申报
+            //IEnumerable<Declaration> stuDecl = _stuDeclRepo.Find(t =>
+            //    t.Term == term &&
+            //    t.ApprovalState == ApprovalState.终审通过
+            //    ).Where(t =>
+            //        !t.Subscription_Id.HasValue
+            //        ).SelectMany(t =>
+            //            t.DeclarationClasses
+            //            ).Where(t =>
+            //                t.ProfessionalClass.School_Id == id
+            //                ).Select(t =>
+            //                    t.Declaration
+            //                    );
 
-            //取未下订单的教师申报
-            IEnumerable<Declaration> teaDecl = _teaDeclRepo.Find(t =>
-                t.Term == term &&
-                t.TeachingTask.Department.School_Id == id &&
-                t.ApprovalState == ApprovalState.终审通过
-                ).Where(t =>
-                    !t.Subscription_Id.HasValue
-                    );
+            ////取未下订单的教师申报
+            //IEnumerable<Declaration> teaDecl = _teaDeclRepo.Find(t =>
+            //    t.Term == term &&
+            //    t.TeachingTask.Department.School_Id == id &&
+            //    t.ApprovalState == ApprovalState.终审通过
+            //    ).Where(t =>
+            //        !t.Subscription_Id.HasValue
+            //        );
 
-            //合并申报
-            var decls = stuDecl.Concat(teaDecl);
-            //创建订单
-            var subs = Domain.SubscriptionService.CreateSubscriptions(decls);
-            subs = subs.OrderBy(t => t.Textbook.Name);
-            var result = _adapter.Adapt<SubscriptionForSubmitView>(subs);
-            return result;
+            ////合并申报
+            //var decls = stuDecl.Concat(teaDecl);
+            ////创建订单
+            //var subs = Domain.SubscriptionService.CreateSubscriptions(decls);
+            //subs = subs.OrderBy(t => t.Textbook.Name);
+            //var result = _adapter.Adapt<SubscriptionForSubmitView>(subs);
+            //return result;
         }
 
         public ResponseView SubmitSubscription(IEnumerable<SubscriptionForSubmitView> subscriptions, string booksellerId, string spareCount)
         {
-            //类型转换
-            var bookseId = booksellerId.ConvertToGuid();
-            var spare = spareCount.ConvertToInt();
-            //学期
-            var term = new TermAppl().GetMaxTerm();
-            //CUD仓储
-            var declRepo = ServiceLocator.Current.GetInstance<IDeclarationRepository>();
-            //操作响应类
-            var result = new ResponseView();
+            return new ResponseView();
+            ////类型转换
+            //var bookseId = booksellerId.ConvertToGuid();
+            //var spare = spareCount.ConvertToInt();
+            ////学期
+            //var term = new TermAppl().GetMaxTerm();
+            ////CUD仓储
+            //var declRepo = ServiceLocator.Current.GetInstance<IDeclarationRepository>();
+            ////操作响应类
+            //var result = new ResponseView();
 
-            try
-            {
-                foreach (var item in subscriptions)
-                {
-                    var subId = item.SubscriptionId.ConvertToGuid();
-                    var textbookId = item.TextbookId.ConvertToGuid();
-                    var planCount = item.DeclarationCount.ConvertToInt();
-                    //创建订单
-                    var sub = Domain.SubscriptionService.CreateSubscription(subId, textbookId, bookseId, term.YearTerm, planCount, spare);
-                    //为申报关联订单
-                    foreach (var declItem in item.Declarations)
-                    {
-                        var id = declItem.DeclarationId.ConvertToInt();
-                        var decl = declRepo.First(t => t.DeclarationId == id);
-                        decl.Subscription = sub;
-                        declRepo.Modify(decl);
-                    }
-                }
+            //try
+            //{
+            //    foreach (var item in subscriptions)
+            //    {
+            //        var subId = item.SubscriptionId.ConvertToGuid();
+            //        var textbookId = item.TextbookId.ConvertToGuid();
+            //        var planCount = item.DeclarationCount.ConvertToInt();
+            //        //创建订单
+            //        var sub = Domain.SubscriptionService.CreateSubscription(subId, textbookId, bookseId, term.YearTerm, planCount, spare);
+            //        //为申报关联订单
+            //        foreach (var declItem in item.Declarations)
+            //        {
+            //            var id = declItem.DeclarationId.ConvertToInt();
+            //            var decl = declRepo.First(t => t.DeclarationId == id);
+            //            decl.Subscription = sub;
+            //            declRepo.Modify(decl);
+            //        }
+            //    }
 
-                declRepo.Context.Commit();
-                return result;
-            }
-            catch (Exception e)
-            {
-                var msg = e.Message;
-                result.IsSuccess = false;
-                result.Message = "征订失败";
-                return result;
-            }
+            //    declRepo.Context.Commit();
+            //    return result;
+            //}
+            //catch (Exception e)
+            //{
+            //    var msg = e.Message;
+            //    result.IsSuccess = false;
+            //    result.Message = "征订失败";
+            //    return result;
+            //}
         }
 
+        #endregion
+
+        #region 辅助方法
+
+        /// <summary>
+        /// 取学年学期未征订的学生用书申报
+        /// </summary>
+        /// <param name="term"></param>
+        /// <returns></returns>
+        protected List<StudentDeclarationJiaoWu> GetNotSubscriptionStudentDeclarationJiaoWu(string term)
+        {
+            //学年学期
+            var yearTerm = new SchoolYearTerm(term);
+            //是否归档等于0
+            var query = _stuDeclJiaoWuRepo.Find(t =>
+                t.SchoolYearTerm.Year == yearTerm.Year &&
+                t.SchoolYearTerm.Term == yearTerm.Term &&
+                t.Sfgd == "0" //0确认征订教材，2确认不要教材，1确认取消教材
+                );
+            //已征订
+            var decl = _stuDeclRepo.Find(t =>
+                t.SchoolYearTerm.Year == yearTerm.Year &&
+                t.SchoolYearTerm.Term == yearTerm.Term
+                );
+            //未征订
+            var result = query.Except(decl);
+            return result.ToList();
+        }
+
+        /// <summary>
+        /// 取学年学期未征订的教师用书申报
+        /// </summary>
+        /// <param name="term"></param>
+        /// <returns></returns>
+        protected List<TeacherDeclarationJiaoWu> GetNotSubscriptionTeacherDeclarationJiaoWu(string term)
+        {
+            //学年学期
+            var yearTerm = new SchoolYearTerm(term);
+
+            //是否归档等于4
+            var query = _teaDeclJiaoWuRepo.Find(t =>
+                t.SchoolYearTerm.Year == yearTerm.Year &&
+                t.SchoolYearTerm.Term == yearTerm.Term &&
+                t.Sfgd == "4" //0申报中，1学院审核中，2教材科审核中，3教务处审核中，4审核完成，A学院审核未通过，B教学建设科审核未通过，C教务处审核未通过
+                );
+            //已征订
+            var decl = _teaDeclRepo.Find(t =>
+                t.SchoolYearTerm.Year == yearTerm.Year &&
+                t.SchoolYearTerm.Term == yearTerm.Term
+                );
+            //未征订
+            var result = query.Except(decl);
+            return result.ToList();
+        }
         #endregion
 
     }
