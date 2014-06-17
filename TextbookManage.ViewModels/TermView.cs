ec02fe676a190389b8ad235e2978ea8576eb3a2e
mlemon <meng004@gmail.com>
2014-06-17 23:43:04 +0800

    修改学期View

--- TextbookManage.ViewModels/TermView.cs
+++ TextbookManage.ViewModels/TermView.cs
@@ -15,13 +15,15 @@
         /// </summary>
         [DataMember]
         public string YearTerm { set; get; }
-
+        /// <summary>
+        /// 学期描述
+        /// </summary>
+        [DataMember]
+        public string Description { get; set; }
         /// <summary>
         /// 当前学年学期
         /// </summary>
         [DataMember]
         public string IsCurrent { set; get; }
-
-
     }
 }
