a29baf230a054eeeea033731fe8eadbcdb8f3380
lemon <meng004@gmail.com>
2014-06-20 11:10:31 +0800

    日志记录器名称

---
+++ TextbookManage.Infrastructure/Logger/LoggerName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextbookManage.Infrastructure.Logger
+{
+    public enum LoggerName
+    {
+        /// <summary>
+        /// 普通记录
+        /// 存放于Log.txt
+        /// </summary>
+        Logger,
+        /// <summary>
+        /// 异常记录
+        /// 存放与ExceptionLog.txt
+        /// </summary>
+        ExceptionLogger
+    }
+}
