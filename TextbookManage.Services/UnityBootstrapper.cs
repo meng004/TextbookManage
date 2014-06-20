1b0a9920fb50f8022e5e9e0cf85cf66b92928543
mlemon <meng004@gmail.com>
2014-06-20 14:29:00 +0800

    dbcontext的生命周期为每线程

--- TextbookManage.Services/UnityBootstrapper.cs
+++ TextbookManage.Services/UnityBootstrapper.cs
@@ -36,7 +36,7 @@ namespace TextbookManage.Services
             container.RegisterType<IBooksellerRepository, BooksellerRepository>()
                 .RegisterType<IFeedbackRepository, FeedbackRepository>()
                 //单例模式，否则报错 一个实体不能被多个上下文跟踪
-                .RegisterType<IRepositoryContext, EntityFrameworkRepositoryContext>(new PerResolveLifetimeManager())
+                .RegisterType<IRepositoryContext, EntityFrameworkRepositoryContext>(new PerThreadLifetimeManager())
                 .RegisterType<IRepository<IAggregateRoot>, EntityFrameworkRepository<IAggregateRoot>>()
                 .RegisterType<ISubscriptionRepository, SubscriptionRepository>()
                 .RegisterType<ITbmisUserRepository, TbmisUserRepository>();
