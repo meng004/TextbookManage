d795311736fb6f7d9e033e1160361ae785f3121e
lemon <meng004@gmail.com>
2014-07-04 16:16:31 +0800

    修改查询条件

--- TextbookManage.WebUI/SubscriptionPlan/OrderQuery.aspx.designer.cs
+++ TextbookManage.WebUI/SubscriptionPlan/OrderQuery.aspx.designer.cs
@@ -157,6 +157,24 @@ namespace TextbookManage.WebUI.SubscriptionPlan {
         protected global::USCTAMis.Web.WebControls.UTMisGrid cgrdPlanSet;
         
         /// <summary>
+        /// cchkSelectAll 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::USCTAMis.Web.WebControls.UTMisCheckBox cchkSelectAll;
+        
+        /// <summary>
+        /// cchkRowCheck 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::USCTAMis.Web.WebControls.UTMisCheckBox cchkRowCheck;
+        
+        /// <summary>
         /// RadWindowManager1 控件。
         /// </summary>
         /// <remarks>
